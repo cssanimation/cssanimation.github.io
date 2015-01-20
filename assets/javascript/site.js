c3643d14f976d25e31e847001a315eddb1df691d
Donovan Hutchinson <donovan@kitmanlabs.com>
2015-01-20 09:07:23 +0000

    Adding touch to demo stuff

---
+++ assets/javascript/site.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  if ($('.tap-to-activate').length > 0) {
+    $('.tap-to-activate').on({
+      'touchstart':
+      function() {
+        $(this).toggleClass('active');
+      }
+    });
+  }
+});
