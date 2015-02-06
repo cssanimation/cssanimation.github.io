403a860cf84f2c2d7504f11be5e73b9809daef70
Donovan Hutchinson <donovan@kitmanlabs.com>
2015-02-06 09:21:03 +0000

    Adding examples to transitions animations article

--- assets/javascript/custom/animations_transitions.js
+++ assets/javascript/custom/animations_transitions.js
@@ -2,5 +2,14 @@ $(function() {
   $('#add-to-list button').click(function() {
     var newLI = $('<li>A new item appears!</li>');
     $('#add-to-list ul').append(newLI);
+    setTimeout(function() {
+      newLI.addClass('show');
+    }, 10);
+    setTimeout(function() {
+      newLI.addClass('hide').text('See ya later :)');
+      setTimeout(function() {
+        newLI.remove();
+      }, 2000);
+    }, 3000);
   });
 });
