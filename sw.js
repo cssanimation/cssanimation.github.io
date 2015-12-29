80cb88107a8ba01011d32e4dbcfe36ab677e79c1
Donovan Hutchinson <donovan@kitmanlabs.com>
2015-12-29 21:38:47 +0000

    Adding a service worker to cache everything

---
+++ sw.js
@@ -0,0 +1,55 @@
+// Service worker to help make the site load more quickly / offline first
+importScripts('javascript/vendor/serviceworker-cache-polyfill.js');
+
+// Cache name - Update to invalidate current cache
+var CACHE_NAME = 'cssanimation-v1';
+
+var urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/javascript/site.js'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event(s)
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
