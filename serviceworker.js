878e6b5c11f52dbc1fe6947c2e5aaadd4f2060fd
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-01-04 08:53:07 +0000

    Updating service worker to delete caches no longer needed

--- serviceworker.js
+++ serviceworker.js
@@ -2,9 +2,11 @@
 importScripts('javascript/vendor/serviceworker-cache-polyfill.js');
 
 // Cache name - Update to invalidate current cache
-var CACHE_NAME = 'cssanimation-css-js-v1';
+var CACHE_NAME = 'install-cache-';
+var VERSION = 'v1';
 
 var urlsToCache = [
+  '/',
   '/css/main.css',
   '/javascript/site.js'
 ];
@@ -13,12 +15,32 @@ var urlsToCache = [
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(CACHE_NAME + VERSION)
       .then(function(cache) {
         return cache.addAll(urlsToCache);
       })
   );
 });
+
+var expectedCaches = [
+  'install-cache-' + VERSION
+];
+
+self.onactivate = function(event) {
+  // remove caches beginning "trains-" that aren't in
+  // expectedCaches
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCaches.indexOf(cacheName) == -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+};
 
 // Fetch event(s)
 self.addEventListener('fetch', function(event) {
