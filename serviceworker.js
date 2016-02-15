40f02ad2b87a45caa8a1eed7cb968ebe9c34af73
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-02-15 19:09:14 +0000

    Removing pygments

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ importScripts('javascript/vendor/serviceworker-cache-polyfill.js');
 
 // Cache name - Update to invalidate current cache
 var CACHE_NAME = 'install-cache-';
-var VERSION = 'v2';
+var VERSION = 'v3';
 
 var urlsToCache = [
   '/',
