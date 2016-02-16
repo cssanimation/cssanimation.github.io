aebdf000e9a853fef5e5853dabd88c8bd6cd7353
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-02-16 13:41:08 +0000

    Fixing baymax link

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ importScripts('javascript/vendor/serviceworker-cache-polyfill.js');
 
 // Cache name - Update to invalidate current cache
 var CACHE_NAME = 'install-cache-';
-var VERSION = 'v3';
+var VERSION = 'v4';
 
 var urlsToCache = [
   '/',
