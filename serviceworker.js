c23da717e896e28edfe1e9c8dc31950a88632379
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-03-10 13:24:12 +0000

    Updating service worker

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ importScripts('javascript/vendor/serviceworker-cache-polyfill.js');
 
 // Cache name - Update to invalidate current cache
 var CACHE_NAME = 'install-cache-';
-var VERSION = 'v8';
+var VERSION = 'v9';
 
 var urlsToCache = [
   '/',
